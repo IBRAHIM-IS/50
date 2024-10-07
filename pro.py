a2242e8d767e24edb2744b2b0a3fecabc151d4c5
IBRAHIM-IS <162879358+IBRAHIM-IS@users.noreply.github.com>
2024-10-07 13:38:46 +0300

    Add files via upload

---
+++ pro.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import numpy as np
+import pickle
+
+st.title("Years of Experience to Salary Predictor")
+
+with open('finalized_model.pickle', 'rb') as file:
+        model = pickle.load(file)
+with open('Scaler.pickle', 'rb') as file_r:
+        scaler = pickle.load(file_r)
+x = st.number_input("Enter Years of Experience:", min_value=0.0, format="%.1f")
+
+if st.button("Predict"):
+    scaled_data = scaler.transform(np.array([[x]]))
+
+    y = model.predict(scaled_data)
+    st.write(f"Predicted Salary: ${y[0]:.2f}")
